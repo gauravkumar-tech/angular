b5c5ce4ca742c2d05f04a0b53a58846c05982d5b
sp1anshul <a.anshul118@gmail.com>
2021-11-21 01:01:18 +0530

    PUSH

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  calculatorform!:FormGroup ;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.calculatorform= new FormGroup({
+      firstnumber1: new FormControl(null),
+      lastname: new FormControl(null),
+      dob: new FormControl(null),
+      mob: new FormControl(null),
+      email: new FormControl(null),
+      sex: new FormControl(null),
   }
 
 }
